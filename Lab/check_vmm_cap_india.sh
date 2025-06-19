34efae8f181f8dca6edcd588cc678a802563f880
mochammad irzan <irzan@juniper.net>
2025-06-19 22:24:12 +0700

    update

--- Lab/check_vmm_cap_india.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in elpod1 elpod2 elpod3 enpod2 enpod4 enpod6 enpod7
-do 
-  export VMM_HOST=${i}-vmm.englab.juniper.net
-  CAP=`ssh -i ~/.ssh/key1  irzan@${VMM_HOST} "vmm capacity -g vmm-default | grep Free"`
-  echo ${VMM_HOST} ${CAP}
-done
--- Lab/virtual_evo_mx_ex/load_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in ce{1..2} p{1..4} 
-do
-	ssh ${i} "edit; load merge relative ${i}.conf; commit"
-done
