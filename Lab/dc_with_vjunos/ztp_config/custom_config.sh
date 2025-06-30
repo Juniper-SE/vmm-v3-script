2d5ab84b431f294ba41364177193a4fbde8725db
mochammad irzan <irzan@juniper.net>
2025-06-30 16:26:16 +0700

    update

--- Lab/dc_with_vjunos/ztp_config/custom_config.sh
+++ Lab/dc_with_vjunos/ztp_config/custom_config.sh
@@ -1,5 +1,5 @@
 cat << EOF | sudo tee /containers_data/tftp/custom_config/junos_custom1.sh
 #!/bin/sh
-cli -c "configure; set system commit synchronize; set chassis evpn-vxlan-default-switch-support; commit and-quit"
+cli -c "configure; set system commit synchronize; set chassis evpn-vxlan-default-switch-support; set chassis fpc 0 pic 0 number-of-ports 12; commit and-quit"
 EOF
 sudo chmod +x /containers_data/tftp/custom_config/junos_custom1.sh
